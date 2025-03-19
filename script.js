8b89b7ce14ffcb45e9231709a10883901eb981cd
Krish <mend0246@student.acitech.org>
2025-03-19 09:14:35 -0400

    Added more stuff like a Restart button Player and CPU

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+    const text = document.getElementById ("text");
+    const rock = document.getElementById ("rock");
+    const container = document.getElementById ("container");
     
+    const btn_list = document.querySelectorAll ("button");
+    console.log(btn_list);
