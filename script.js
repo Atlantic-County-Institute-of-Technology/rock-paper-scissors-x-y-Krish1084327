c6ea2fca6d464673da6e2c34e73d84824d2745ca
Krish <mend0246@student.acitech.org>
2025-03-24 09:13:04 -0400

    Need to add more to my javascript and finished css

--- script.js
+++ script.js
@@ -2,13 +2,30 @@
     const rock = document.getElementById ("1");
     const paper = document.getElementById ("2");
     const scissors = document.getElementById ("3");
+    const pencil = document.getElementById ("4");
+    const telephone = document.getElementById ("5");
     const container = document.getElementById ("container");
     
     const btn_list = document.querySelectorAll ("button");
     console.log(btn_list);
+    
+    function winner(player, computer) {
 
-    if (player chose scissors)
-    if (cpu chose paper or cpu chose telephone)
-      "Player Wins!"
-      else if(cpu chose rock or pencil)
-      "Player Loses!"
+      player = player.toLowerCase();
+      computer = computer.toLowerCase();
+      if (player === computer) {
+          result.textContent = 'Tie'
+      }
+      else if (player.localeCompare('1')) {
+          if (computer == '1' || computer == '5' || computer == '4') {
+              result.textContent = 'Computer Won';
+              computerScore++;
+              computerScoreBoard.textContent = computerScore;
+
+          } else {
+              result.textContent = 'Player Won'
+              playerScore++;
+              playerScoreBoard.textContent = playerScore;
+          }
+      }
+    }
